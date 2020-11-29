466f1442112b0bf1546caa6ffde2bd0a884f3426
edwardhyde <komesergey@gmail.com>
2020-11-29 14:54:11 +0300

    Added translation example

--- summarization.py
+++ summarization.py
@@ -19,7 +19,7 @@ context = "The Apollo program, also known as Project Apollo, was the third Unite
 gpu = torch.device('cuda')
 model = T5ForConditionalGeneration.from_pretrained('t5-large').to(gpu)
 tokenizer = T5Tokenizer.from_pretrained('t5-base')
-tokens_input = tokenizer.encode("summarize: " + context, return_tensors="pt", max_length=1024, truncation=True)
+tokens_input = tokenizer.encode(text="summarize: " + context, return_tensors="pt", max_length=1024, truncation=True)
 summary_ids = model.generate(tokens_input.to(gpu), min_length=60, max_length=180, length_penalty=4.0)
 summary = tokenizer.decode(summary_ids[0])
 
