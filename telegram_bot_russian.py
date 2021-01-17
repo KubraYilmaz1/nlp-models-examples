f49decfd57aebf17acc9268a9f065cee0ef9d4ad
dredwardhyde <komesergey@gmail.com>
2021-01-17 19:45:46 +0300

    Refactoring

--- telegram_bot_russian.py
+++ telegram_bot_russian.py
@@ -16,7 +16,7 @@ REQUEST_KWARGS = {
         'password': 'PASSWORD',
     }
 }
-length = 22
+length = 100
 repetition_penalty = 1.5
 temperature = 1
 
@@ -24,16 +24,21 @@ temperature = 1
 def reply(update, context):
     tokens = tokenizer.encode(update.message.text)
     for _ in range(length):
-        logits = model(torch.tensor([tokens], device=gpu))[0]
-        next_token_logits = logits[0, -1, :] / temperature
-        for token in set(tokens):
-            next_token_logits[token] /= repetition_penalty
-        tokens += [torch.argmax(next_token_logits).item()]
+        with torch.no_grad():
+            logits = model(torch.tensor([tokens], device=gpu))[0]
+            next_token_logits = logits[0, -1, :] / temperature
+            for token in set(tokens):
+                next_token_logits[token] /= repetition_penalty
+            values, indices = torch.topk(torch.softmax(next_token_logits, dim=-1), 5)
+            all_sum = torch.sum(values)
+            values /= all_sum
+            index = torch.multinomial(values, 1).item()
+            tokens += [indices[index].item()]
     context.bot.send_message(chat_id=update.effective_chat.id, text=tokenizer.decode(tokens))
 
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-updater = Updater(TOKEN, request_kwargs=REQUEST_KWARGS, use_context=True)
+updater = Updater(TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 echo_handler = MessageHandler(Filters.text & (~Filters.command), reply)
 dispatcher.add_handler(echo_handler)
