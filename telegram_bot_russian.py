5fd3f700612138238dccea1e41c1380f4ce1df2b
dredwardhyde <komesergey@gmail.com>
2021-01-17 20:18:08 +0300

    Refactoring

--- telegram_bot_russian.py
+++ telegram_bot_russian.py
@@ -3,7 +3,7 @@ import logging
 import torch
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
 from telegram.ext import Updater, MessageHandler, Filters
-
+import torch.nn.functional as F
 gpu = torch.device('cuda')
 tokenizer = GPT2Tokenizer.from_pretrained("sberbank-ai/rugpt3large_based_on_gpt2")
 model = GPT2LMHeadModel.from_pretrained("sberbank-ai/rugpt3large_based_on_gpt2").to(device=gpu)
@@ -19,6 +19,7 @@ REQUEST_KWARGS = {
 length = 100
 repetition_penalty = 1.5
 temperature = 1
+threshold = 0.1
 
 
 def reply(update, context):
@@ -29,11 +30,15 @@ def reply(update, context):
             next_token_logits = logits[0, -1, :] / temperature
             for token in set(tokens):
                 next_token_logits[token] /= repetition_penalty
-            values, indices = torch.topk(torch.softmax(next_token_logits, dim=-1), 5)
-            all_sum = torch.sum(values)
-            values /= all_sum
-            index = torch.multinomial(values, 1).item()
-            tokens += [indices[index].item()]
+
+            sorted_logits, sorted_indices = torch.sort(torch.softmax(next_token_logits, dim=-1), descending=True)
+            cumulative_probs = torch.cumsum(F.softmax(sorted_logits, dim=-1), dim=-1)
+            sorted_indices_to_remove = cumulative_probs > 0.5
+            sorted_indices_to_remove[..., 1:] = sorted_indices_to_remove[..., :-1].clone()
+            sorted_indices_to_remove[..., 0] = 0
+            indices_to_remove = sorted_indices[sorted_indices_to_remove]
+            next_token_logits[indices_to_remove] = -float('Inf')
+            tokens += [torch.multinomial(F.softmax(next_token_logits, dim=-1), num_samples=1).item()]
     context.bot.send_message(chat_id=update.effective_chat.id, text=tokenizer.decode(tokens))
 
 
